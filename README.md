1a759e1d48152cc954c7fc21f6c2e73100a4c6d2
Santi <santiagomarinmarin219@gmail.com>
2025-04-21 14:47:16 -0500

    Primeros cambios e implemetancion de "Includes"
    En este primer cambio implemente los "Includes", cambie el nombre de "Venta" a "Compra" e hice un cambio en el "README.md"

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # seguridad_web
 
 ## 👥 Integrantes del equipo
-- Juan José Giraldo Patiño  
-- Juan Fernando Grisalez  
+
+- Juan José Giraldo Patiño
+- Juan Fernando Grisalez
 - Santiago Marín Marín
 
 ## 🛒 Ecommerce con validaciones de seguridad en los formularios
@@ -10,32 +11,33 @@
 Este sitio web está conformado por dos formularios, ambos con validaciones y protecciones para evitar ataques de **inyección SQL**.
 
 ### 📄 Formulario 1: Registro de usuario
-- Nombre  
-- Edad  
-- Correo  
-- Contraseña  
-- Confirmar contraseña  
-- Teléfono  
+
+- Nombre
+- Edad
+- Correo
+- Contraseña
+- Confirmar contraseña
+- Teléfono
 - Fecha de cumpleaños
 
 ### 📄 Formulario 2: Registro de compra
-- Dirección de envío  
-- Métodos de pago  
-- Producto  
-- Cantidad de productos
+
+- Producto
+- Cantidad de producto
+- Medio de pago
+- Dirección de envío
 
 ## 🏠 Página de inicio
 
 La página de inicio mostrará los productos del ecommerce. Cada producto incluirá:
 
-- Imagen  
-- Precio  
-- Nombre del producto  
+- Imagen
+- Precio
+- Nombre del producto
 
 Al hacer clic en un producto, se abrirá una **ventana exclusiva** con toda la información detallada. En esa vista el usuario podrá:
 
-- Comprar el producto  
-- Volver a la página de inicio  
+- Comprar el producto
+- Volver a la página de inicio
 
 🔒 **Nota**: El usuario no podrá realizar compras sin estar registrado. Si intenta hacerlo sin haberse registrado previamente, será redirigido automáticamente al formulario de registro.
-
---
+++ includes/Header.php
@@ -0,0 +1,53 @@
+<header>
+<nav class="navbar navbar-expand-sm" style="background-color: #eeeae2">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"
+          ><img src="Img/logo.png" height="100px" width="100px"
+        /></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav gap-5">
+            <li class="nav-item ms-4">
+              <a class="nav-link" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link active fw-semibold"
+                aria-current="page"
+                href="#"
+                >Compra</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Registro</a>
+            </li>
+            <li class="ms-5 me-5">
+              <input
+                class="form-control rounded-pill"
+                style="width: 400px"
+                type="search"
+                placeholder="Buscar"
+                aria-label="Search"
+              />
+            </li>
+            <li class="nav-item ms-5">
+              <a class="nav-link" href="#">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Quienes Somos </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</header>
