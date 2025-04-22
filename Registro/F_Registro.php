d26c362639d5fc74804335482b1071be2d235e4e
juan <jofejuanfer133@gmail.com>
2025-04-21 20:42:04 -0500

    formulario registro
    se creo el formulario con los campos correspondientes, se creo el archivo de validaciones donde estan las reglas de como el usuario debe registrar los datos y si esta mal le aparece un mensaje de como debe ser, se creo el archivo de registro php donde estan los enlaces a la base de datos para que se envien correctamente los datos , y por ultimo el archivo de bienvenido donde al registrarse lo manda a la pagina de bienvenido y desde ahi el se puede ir al inicio

---
+++ Registro/F_Registro.php
@@ -0,0 +1,143 @@
+<?php
+$conexion = CreateConnection();
+if ($conexion->connect_error) {
+    echo "Error en la conexión a la base de datos";
+    die("Error en la conexión a la base de datos: " . $conexion->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header("Location: F_Registro.html");
+    echo "Post";
+    exit();
+}
+
+// Obtener los datos enviados desde el formulario
+$nombre_completo = cleanInput($_POST['nombre_completo']);
+$edad = cleanInput($_POST['edad']);
+$telefono = cleanInput($_POST['telefono']);
+$fecha_nacimiento = cleanInput($_POST['fecha_nacimiento']);
+$correo = cleanInput($_POST['correo']);
+$contrasena = cleanInput($_POST['contrasena']);
+$repetir_contrasena = cleanInput($_POST['repetir_contrasena']);
+
+// Validar que los datos no estén vacíos
+if (empty($nombre_completo) || empty($correo) || empty($contrasena) || empty($edad) || empty($telefono) || empty($fecha_nacimiento)) {
+    header("Location: F_Registro.html");
+    echo "llegaron";
+    exit();
+}
+// Validar que el correo electrónico tenga un formato válido
+if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+    header("Location: F_Registro.html");
+    exit();
+}
+
+if ($contrasena !== $repetir_contrasena) {
+    $_SESSION['error'] = "Las contraseñas no coinciden";
+    header("Location: F_Registro.html");
+    exit();
+}
+try {
+    //Generar sal para agregar el factor de pseudoaleatoriedad
+    $salt = random_bytes(16); // Genera un salt aleatorio de 32 caracteres hexadecimales
+    $salt_hex = bin2hex($salt); // Convierte el salt a hexadecimal
+
+    $password_with_salt = $contrasena . $salt_hex; // Combina la contraseña con el salt
+
+    $hashed_password = hash('sha256', $password_with_salt); // Hashea la contraseña con el salt
+
+    // Código vulnerable a inyección SQL
+    $sql = "INSERT INTO clientes (nombre_completo, correo, edad, fecha_cumpleanos, contrasena, telefono, salt) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    // Preparar la consulta
+    $stmt = $conexion->prepare($sql);
+
+    if (!$stmt) {
+        redirectToRegister();
+        exit();
+    }
+
+    // Ejecutar la consulta directamente sin preparar ni sanitizar
+
+
+    $stmt->bind_param(
+        "ssissss", // s: string, i: int
+        $nombre_completo,
+        $correo,
+        $edad,
+        $fecha_nacimiento,
+        $hashed_password,
+        $telefono,
+        $salt_hex
+    );
+
+    // Ejecutar la consulta
+    // Aquí se ejecuta la consulta SQL, lo que puede ser vulnerable a inyección SQL
+
+    if ($stmt->execute()) {
+        redirectToWelcomePage();
+    } else {
+        redirectToRegister();
+    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+
+
+// Verificar si se registró el usuario
+
+
+$conexion->close();
+
+/**
+ * Redirige al usuario a la página de bienvenida.
+ */
+function redirectToWelcomePage()
+{
+    header("Location: bienvenido.php");
+    exit();
+}
+
+/**
+ * Redirige al usuario a la página de login.
+ */
+function redirectToRegister()
+{
+    header("Location: registro.html");
+    exit();
+}
+
+// Función para limpiar la entrada del usuario
+// Esta función es vulnerable a inyección SQL y no debería usarse en producción
+function cleanInput($input)
+{
+    // Eliminar espacios en blanco al inicio y al final
+    $input = trim($input);
+    // Eliminar barras invertidas
+    $input = stripslashes($input);
+    // Convertir caracteres especiales a entidades HTML
+    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+    return $input;
+}
+
+
+
+// Crear conexión a la base de datos
+function CreateConnection()
+{
+    try {
+        // Datos de conexión a la base de datos
+        $host = "localhost";
+        $usuario_db = "security";
+        $contrasena_db = "security";
+        $nombre_db = "seguridad";
+
+        // Conexión a la base de datos
+        $conexion = new mysqli($host, $usuario_db, $contrasena_db, $nombre_db);
+        return $conexion;
+    } catch (Exception $ex) {
+        echo "Error en la conexión a la base de datos" . $ex->getMessage();
+        exit();
+    }
+}
---
+++ Registro/ValidacionesR.js
@@ -0,0 +1,97 @@
+// ValidacionesR.js
+// Definir la regla personalizada primero
+$.validator.addMethod("maxDateToday", function(value, element) {
+    const today = new Date().toISOString().split('T')[0];
+    return value <= today;
+}, "La fecha de nacimiento no puede ser mayor a la fecha actual");
+
+// Configurar jQuery Validation
+// $.validator.setDefaults({
+//   // submitHandler: function () {
+//   //   alert("submitted!");
+//   // },
+// });
+
+$(document).ready(function () { 
+  $("#signupForm").validate({
+    rules: { 
+      nombre_completo: {
+        required: true,
+        minlength: 5,
+      },
+      edad: {
+        required: true,
+        //=>12
+      },
+      telefono: {
+        required: true,
+        minlength: 10,
+        maxlength : 15,
+      },
+      fecha_nacimiento: {
+        required: true,
+        maxDateToday: true, // <-- Solo true aquí
+        //=> 12 años
+      },
+      contrasena: {
+        required: true,
+        minlength: 8,
+      },
+      repetir_contrasena: {
+        required: true,
+        minlength: 8,
+        equalTo: "#contrasena",
+        
+      },
+      correo: {
+        required: true,
+        email: true,
+      },
+      
+    },
+    messages: {
+      nombre_completo: {
+        required: "Por favor ingresa tu nombre completo",
+        minlength: "Tu nombre debe ser de no menos de 5 caracteres",
+      },
+      edad: {
+        required: "Por favor ingresa tu edad",
+      },
+      telefono: {
+        required: "Por favor ingresa tu número de teléfono",
+        minlength: "Tu número de teléfono debe ser de no menos de 10 caracteres",
+        maxlength: " tu número no puede pasar de 15 caracteres "
+      },
+      fecha_nacimiento: {
+        required: "Por favor ingresa tu fecha de nacimiento",
+        maxDateToday: "La fecha de nacimiento no puede ser mayor a la fecha actual",
+      },
+      contrasena: {
+        required: "Por favor ingresa una contraseña",
+        minlength: "Tu contraseña debe ser de no menos de 8 caracteres de longitud",
+      },
+      repetir_contrasena: {
+        required: "Ingresa otra vez tu contraseña",
+        minlength: "Tu contraseña debe ser de no menos de 5 caracteres de longitud",
+        equalTo: "Por favor ingresa la misma contraseña de arriba",
+      },
+      correo: "Por favor ingresa un correo válido",
+    },
+
+    errorElement: "em",
+    errorPlacement: function (error, element) {
+      error.addClass("help-block");
+      if (element.prop("type") === "checkbox") {
+        error.insertAfter(element.parent("label"));
+      } else {
+        error.insertAfter(element);
+      }
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass("is-invalid").removeClass("is-valid");
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).addClass("is-valid").removeClass("is-invalid");
+    },
+  });
+});
