1c8656bcffb2e12a4aaca048ae46cf2d184edce2
juan <jofejuanfer133@gmail.com>
2025-05-23 15:58:28 -0500

    mejora enums

--- pr_token/admin.php
+++ pr_token/admin.php
@@ -10,18 +10,18 @@ $secret_key = "clave_secreta_super_segura";
 
 // Verifica el token y el rol
 if (!isset($_SESSION['token'])) {
-    header("Location: acceso_no_autorizado.php");
+    header(GeneralConfig::accesoNoAutorizado->value);
     exit;
 }
 
 try {
-    $decoded = JWT::decode($_SESSION['token'], new Key($secret_key, 'HS256'));
+    $decoded = JWT::decode($_SESSION['token'], new Key($secret_key, GeneralConfig::encryptTokenAlgorith->value));
     if ($decoded->rol !== 'admin') {
-         header("Location: accesodenegado.php");
+         header(GeneralConfig::accesoDenegado->value);
         exit;
     }
 } catch (Exception $e) {
-    header("Location: ../Inicio/Inicio.php");
+    header(GeneralConfig::Inicio->value);
     exit;
 }
 ?>
